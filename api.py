ca97e87ced3fba1dcbc826cbcc40871fb263d33c
umihico <umihico_dummy@users.noreply.github.com>
2018-11-13 07:06:49 +0900

    stable downloading gifs

---
+++ api.py
@@ -0,0 +1,76 @@
+
+import requests
+import pprint
+import time
+import datetime
+
+
+def _created_range_iter():
+    for year in range(2014, 9999):
+        for month in [1, 4, 7, 10]:
+            today = datetime.date.today()
+            if datetime.datetime(year, month, 1) > datetime.datetime(today.year, today.month, today.day):
+                raise StopIteration
+            end_year = year if month != 10 else year + 1
+            end_month = month + 3 if month != 10 else 1
+            created_range = f"created:{year}-{str(month).zfill(2)}-01..{end_year}-{str(end_month).zfill(2)}-01"
+            yield created_range
+
+
+def _test_created_range_iter():
+    for created_range in _created_range_iter():
+        print(created_range)
+
+
+def _json_iter(topic="portfolio-website"):
+    last_GET_time = 0
+    for created_range in _created_range_iter():
+        for i in range(1, 9999):
+            headers = {
+                'Accept': 'application/vnd.github.mercy-preview+json', }
+            url = f'https://api.github.com/search/repositories?q=topic:{topic}+{created_range}&page={i}&per_page=100'
+            print("GET", url)
+            time.sleep(5 - min([time.time() - last_GET_time, 0]))
+            last_GET_time = time.time()
+            response = requests.get(url, headers=headers)
+            json = response.json()
+            # pprint.pprint(json)
+            json["url"] = response.url
+            total_count = json['total_count']
+
+            yield json
+            if i > total_count // 100:
+                break
+
+
+def _test_json_iter():
+    for json in _json_iter():
+        print(json['url'])
+        print(json)
+
+
+def iter_repo(topic="portfolio-website"):
+    urlset = set()
+    for json in _json_iter():
+        total_count, repos = json['total_count'], json['items']
+        for repo in repos:
+            if repo['html_url'] in urlset:
+                continue
+            urlset.add(repo['html_url'])
+            if repo['homepage']:
+                yield repo
+            else:
+                print("no homepage", repo['html_url'])
+
+
+def _test_iter_repo():
+    for repo in iter_repo():
+        html_url, description, homepage, created_at, score, stargazers_count = repo['html_url'], repo[
+            'description'], repo['homepage'], repo['created_at'][:10], repo['score'], repo['stargazers_count']
+        print(created_at, html_url, description, homepage, stargazers_count)
+
+
+if __name__ == '__main__':
+    # _test_created_range_iter()
+    # _test_json_iter()
+    _test_iter_repo()
---
+++ scraping.py
@@ -0,0 +1,60 @@
+from selenium.webdriver import Chrome, ChromeOptions
+import requests
+from PIL import Image
+from io import BytesIO
+import time
+
+
+def gen_chrome():
+    options = ChromeOptions()
+    options.add_argument("--start-maximized")
+    return Chrome(chrome_options=options)
+
+
+def _gen_images_for_gif(url, chrome=None):
+
+    chrome = chrome or gen_chrome()
+    try:
+        print("chrome.get", url)
+        chrome.get(url)
+        requests.get(url).raise_for_status()
+    except Exception as e:
+        print(url)
+        raise
+    images = []
+    h = 0
+    for scroll in range(999):
+        height = chrome.execute_script("return document.body.scrollHeight")
+        if h > height:
+            chrome.execute_script(f"window.scrollTo(0, {h});")
+            new_height = chrome.execute_script(
+                "return document.body.scrollHeight")
+            if new_height == height:
+                break
+        png = chrome.get_screenshot_as_png()
+        image = Image.open(BytesIO(png))
+        image.thumbnail((300, 300), Image.LANCZOS)
+        images.append(image)
+        if scroll > 2:
+            h += 400
+        chrome.execute_script(f"window.scrollTo(0, {h});")
+        time.sleep(1)
+    return images
+
+
+def _test(url="http://www.albinotonnina.com/"):
+    filename = 'test.gif'
+    url_to_gif(url, filename)
+
+
+def url_to_gif(url, filename, chrome=None):
+    images = _gen_images_for_gif(url, chrome)
+    images[0].save(filename, save_all=True,
+                   append_images=images[1:], duration=600, loop=100, quality=30, optimize=True)
+
+
+if __name__ == '__main__':
+    chrome = gen_chrome()
+    chrome.get("https://dargueta.com")
+    raise
+    _test(url="https://ryanfitzgerald.github.io/devportfolio")
