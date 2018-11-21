564457ab708bac5b246185184d73938af0982c15
umihico <umihico_dummy@users.noreply.github.com>
2018-11-21 16:24:14 +0900

    db update

--- download.py
+++ download.py
@@ -37,7 +37,7 @@ def should_gif_download(repo):
     if not os.path.exists(filename):
         # print("no gif yet", full_name)
         return True
-    if _conv_updated_at_comparable(repo['updated_at']) > _conv_updated_at_comparable(db_repo['updated_at']) and and repo['db_updated_at'] - db_repo['db_updated_at'] > 60 * 60 * 24:
+    if _conv_updated_at_comparable(repo['updated_at']) > _conv_updated_at_comparable(db_repo['updated_at']) and time.time() - db_repo.get('db_updated_at', 0) > 60 * 60 * 24:
 
         # print("github updated", full_name)
         return True
--- no_thanks.py
+++ no_thanks.py
@@ -3,7 +3,7 @@ For privacy reason, please insert the sha256 hash of your name(lowercase) instea
 in bash
 $echo -n $(echo -n umihico|awk '{print tolower($0)}')|sha256sum
 >> 3c32bdae91be9d7718f2bed48b0b5202fcd4da2b0a26959eea9235d2417a9e5c  -
-or print(common.hash_username("jeroentvb")) in my repository
+or print(common.hash_username("umihico")) in my repository
 so please insert '3c32bdae91be9d7718f2bed48b0b5202fcd4da2b0a26959eea9235d2417a9e5c' in this case.
 If you don't wanna take time, just create issue or give me message on twitter @umihico_ (underscore is required)
 Thank you!
