c4d2619200d30a92593dbc2e3129d3ff3f2e568c
umihico <umihico@users.noreply.github.com>
2019-01-21 21:07:36 +0900

    start using chromeless

--- scripts/scraping.py
+++ scripts/scraping.py
@@ -1,91 +1,93 @@
-from selenium.webdriver import Chrome, ChromeOptions
 import requests
 from PIL import Image
 from io import BytesIO
 import time
-import traceback
-
-global_chrome_box = [None]
-
-
-def gen_chrome():
-    options = ChromeOptions()
-    # options.add_argument("--start-maximized")
-    options.add_argument("--disable-infobars")
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=2160,1440')
-    chrome = Chrome(chrome_options=options)
-    chrome.set_page_load_timeout(30)
-    return chrome
+from chromeless import Chromeless
+from selenium.webdriver import ChromeOptions
+from awsgateway_credentials import awsgateway_apikey, awsgateway_url
 
 
-def _gen_images_for_gif(url):
-    if global_chrome_box[0] is None:
-        global_chrome_box[0] = gen_chrome()
-        chrome = global_chrome_box[0]
-    try:
-
-        images = _gen_images_for_gif_main(url, chrome)
-    except Exception as e:
-        try:
-            chrome.quit()
-        except Exception as e:
-            pass
-        global_chrome_box[0] = None
-        return False
-    else:
-        return images
-
-
-def _gen_images_for_gif_main(url, chrome):
-    try:
-        requests.get(url, timeout=10).raise_for_status()
-    except Exception as e:
-        # print("bad url", url)
-        return False
-    # print("chrome.get", url)
-    chrome.get(url)
-    images = []
-    h = 0
-    for scroll in range(999):
-        height = chrome.execute_script("return document.body.scrollHeight")
-        if h > height:
-            chrome.execute_script(f"window.scrollTo(0, {h});")
-            new_height = chrome.execute_script(
-                "return document.body.scrollHeight")
-            if new_height == height:
-                break
-        png = chrome.get_screenshot_as_png()
-        image = Image.open(BytesIO(png))
-        image.thumbnail((500, 500), Image.LANCZOS)
-        images.append(image)
-        if scroll > 2:
-            h += 400
-        chrome.execute_script(f"window.scrollTo(0, {h});")
-        time.sleep(1)
-    return images
+def gen_chrome_options():
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1366x768")
+    chrome_options.add_argument("--disable-application-cache")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--hide-scrollbars")
+    chrome_options.add_argument("--enable-logging")
+    chrome_options.add_argument("--log-level=0")
+    chrome_options.add_argument("--single-process")
+    chrome_options.add_argument("--ignore-certificate-errors")
+    chrome_options.add_argument("--homedir=/tmp")
+    return chrome_options
 
 
-def _test(url="http://www.albinotonnina.com/"):
-    filename = 'test.gif'
+chrome_options = gen_chrome_options()
+
+
+def test_url_to_gif(url="http://umihi.co/", filename='test.gif'):
     url_to_gif(url, filename)
 
 
+def scroll_each_iter(self):
+    h = 0
+    import time
+    for scroll in range(999):
+        height = self.execute_script("return document.body.scrollHeight")
+        if h > height:
+            self.execute_script(f"window.scrollTo(0, {h});")
+            new_height = self.execute_script(
+                "return document.body.scrollHeight")
+            if new_height == height:
+                break
+        if scroll > 2:
+            h += 400
+        self.execute_script(f"window.scrollTo(0, {h});")
+        time.sleep(0.5)
+        yield
+
+
+def scrolling_capture(self, url):
+    import time
+    starttime = time.time()
+    self.get(url)
+    pngs = []
+    for _ in self.scroll_each_iter():
+        if time.time()-starttime > 20:
+            break
+        pngs.append(self.get_screenshot_as_png())
+    return pngs
+
+
 def url_to_gif(url, filename):
-    images = _gen_images_for_gif(url)
+    try:
+        requests.get(url, timeout=10).raise_for_status()
+    except Exception as e:
+        gif_success = False
+        return gif_success
+    chrome = Chromeless(awsgateway_url, awsgateway_apikey, chrome_options=chrome_options)
+    chrome.attach_method(scrolling_capture)
+    chrome.attach_method(scroll_each_iter)
+    try:
+        pngs = chrome.scrolling_capture(url)
+    except Exception as e:
+        gif_success = False
+        return gif_success
+    images = []
+    for png in pngs:
+        image = Image.open(BytesIO(png))
+        image.thumbnail((500, 500), Image.LANCZOS)
+        images.append(image)
     gif_success = bool(images)
     if gif_success:
         images[0].save(filename, save_all=True,
                        append_images=images[1:], duration=600, loop=100, quality=30, optimize=True)
-    if gif_success:
         print('saved', filename)
     return gif_success
 
 
 if __name__ == '__main__':
-    chrome = gen_chrome()
-    chrome.get("https://dargueta.com")
-    raise
-    _test(url="https://ryanfitzgerald.github.io/devportfolio")
+    test_url_to_gif("https://ryanfitzgerald.github.io/devportfolio/",
+                    filename="/home/umihico/git-powered-philosophy/worktree-draft/site/test.gif")
