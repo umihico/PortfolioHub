a694f201e2d5f12a5cc6e09033ce57c9768edec8
umihico <umihico@users.noreply.github.com>
2020-02-15 08:55:40 +0900

    switch dotenv based on ENV

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app->loadEnvironmentFrom('.env.'.((getenv("ENV")) ? getenv("ENV") : "local"));
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ getenv("APP_ENV") }}
                 </div>
 
                 <div class="links">
