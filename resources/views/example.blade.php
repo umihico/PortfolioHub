0884d32ab97bc6810c4bcb132729613893d15823
umihico <umihico@users.noreply.github.com>
2020-02-15 09:25:26 +0900

    confirm vue works

---
+++ resources/views/example.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <example-component></example-component>
+        </div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/example', function () {
+    return view('example');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
