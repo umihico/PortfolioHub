087e3957e327e7492d835d0fda973f685ff80e69
umihico <umihico@users.noreply.github.com>
2020-08-17 08:07:45 +0900

    lint *.vue

--- resources/js/components/CardsContent.vue
+++ resources/js/components/CardsContent.vue
@@ -1,16 +1,17 @@
 <template>
     <div>
         <v-container>
-        <v-autocomplete
-            v-model="location"
-            v-if="locations.length"
-            :items="locations"
-            @input="push()"
-            clearable
-            chips
-            label="Location filter"
-            solo
-        ></v-autocomplete></v-container>
+            <v-autocomplete
+                v-model="location"
+                v-if="locations.length"
+                :items="locations"
+                @input="push()"
+                clearable
+                chips
+                label="Location filter"
+                solo
+            ></v-autocomplete>
+        </v-container>
         <div class="text-center">
             <pagination :page="parseInt(page)" @pageHandler='set_page' ref="header_pagination"></pagination>
         </div>
@@ -27,13 +28,14 @@
     </div>
 </template>
 <script>
-    let load_portfolio=function(app){
-        let query={page:parseInt(app.page), location:app.location};
+
+    let load_portfolio = function (app) {
+        let query = {page: parseInt(app.page), location: app.location};
         axios.post(`/api/fetch_portfolio_ids?_=${new Date().getTime()}`, query).then(function (res) {
 
             console.log(res.data);
-            if(res.data.data.length==0 && app.page>1){
-                app.page=res.data.last_page;
+            if (res.data.data.length == 0 && app.page > 1) {
+                app.page = res.data.last_page;
                 app.push();
             }
             app.set_portfolios(res.data);
@@ -44,9 +46,10 @@
     };
     import portfolio_card from './PortfolioCard.vue';
     import pagination from './pagination.vue';
+
     export default {
-        name:"cards",
-        components:{
+        name: "cards",
+        components: {
             portfolio_card,
             pagination,
         },
@@ -55,49 +58,49 @@
                 locations: [],
                 location: "",
                 value: null,
-                page:1,
+                page: 1,
             };
         },
         watch: {
-            '$route' (to, from) {
-                this.page=parseInt(to.query.page); // setting variables when going back
-                this.$refs['header_pagination'].data_page=this.page;
-                this.$refs['footer_pagination'].data_page=this.page;
-                this.location=to.query.location; // setting variables when going back
+            '$route'(to, from) {
+                this.page = parseInt(to.query.page); // setting variables when going back
+                this.$refs['header_pagination'].data_page = this.page;
+                this.$refs['footer_pagination'].data_page = this.page;
+                this.location = to.query.location; // setting variables when going back
                 load_portfolio(this);
             }
         },
         methods: {
-            set_portfolios(data){
+            set_portfolios(data) {
                 for (let i = 0; i < 12; i++) {
-                    if(i < data.data.length){
+                    if (i < data.data.length) {
                         this.$refs['portfolio_card'][i].set(data.data[i]);
-                    }else{
+                    } else {
                         this.$refs['portfolio_card'][i].clear();
                     }
                 }
                 this.$refs['header_pagination'].show(data.last_page);
                 this.$refs['footer_pagination'].show(data.last_page);
             },
-            set_page (msg) {
+            set_page(msg) {
                 // receiving message(page) from pagination component
                 this.page = msg;
                 this.push();
             },
-            push(){
-                let query={page:this.page};
-                if(this.location)query.location=this.location;
-                this.$router.push({ path: "/", query:query});
-                window.history.replaceState({}, '',"/?"+Object.keys(query).map(key => `${key}=${encodeURIComponent(query[key])}`).join('&'));
+            push() {
+                let query = {page: this.page};
+                if (this.location) query.location = this.location;
+                this.$router.push({path: "/", query: query});
+                window.history.replaceState({}, '', "/?" + Object.keys(query).map(key => `${key}=${encodeURIComponent(query[key])}`).join('&'));
             }
         },
-        created(){
-            this.location=this.$route.query.location;
-            this.page=(this.$route.query.page) ? parseInt(this.$route.query.page) : 1;
+        created() {
+            this.location = this.$route.query.location;
+            this.page = (this.$route.query.page) ? parseInt(this.$route.query.page) : 1;
             load_portfolio(this);
-            let app=this;
+            let app = this;
             axios.get(`/api/fetch_geotags?_=${new Date().getTime()}`).then(function (res) {
-                app.locations=res.data;
+                app.locations = res.data;
             }).catch(function (error) {
                 console.log(error);
             }).finally(function () {
--- resources/js/components/PortfolioCard.vue
+++ resources/js/components/PortfolioCard.vue
@@ -1,58 +1,63 @@
 <template>
     <transition name="fade" mode="out-in">
-    <v-card
-        @mouseenter="hovered=true"
-        @mouseleave="hovered=false"
-        class="ma-4"
-        v-if="gif"
-    >
-        <a :href="'https://github.com/'+user" target="_blank" style="color: inherit; text-decoration: none;">
-            <v-card-title class="font-weight-black">
-            <v-avatar size="50px" class="ma-1" v-if="user">
-                <img
-                    :src="'https://avatars.githubusercontent.com/'+user+'?s=50'"
+        <v-card
+            @mouseenter="hovered=true"
+            @mouseleave="hovered=false"
+            class="ma-4"
+            v-if="gif"
+        >
+            <a :href="'https://github.com/'+user" target="_blank" style="color: inherit; text-decoration: none;">
+                <v-card-title class="font-weight-black">
+                    <v-avatar size="50px" class="ma-1" v-if="user">
+                        <img
+                            :src="'https://avatars.githubusercontent.com/'+user+'?s=50'"
+                        >
+                    </v-avatar>
+                    {{ user }}
+                </v-card-title>
+            </a>
+            <a target="_blank" :href="url" :key="gif">
+                <v-img
+                    class="white--text align-end"
+                    width="100%"
+                    :src="gif"
                 >
-            </v-avatar>
-            {{ user }}</v-card-title>
-        </a>
-        <a target="_blank" :href="url" :key="gif">
-            <v-img
-                class="white--text align-end"
-                width="100%"
-                :src="gif"
-            >
-            </v-img>
-        </a>
-        <v-card-text class="text--primary">
-            <div class="" style="display: flex;">
-                <template v-for="(ratio,lang) in skills">
-                    <p class="color-bar" :style="'height: 7px;background-color: '+String((color_dict[lang]) ? color_dict[lang].color : 'red')+'; width: '+ratio+'%'"></p>
+                </v-img>
+            </a>
+            <v-card-text class="text--primary">
+                <div class="" style="display: flex;">
+                    <template v-for="(ratio,lang) in skills">
+                        <p class="color-bar"
+                           :style="'height: 7px;background-color: '+String((color_dict[lang]) ? color_dict[lang].color : 'red')+'; width: '+ratio+'%'"></p>
+                    </template>
+                </div>
+                <template v-for="(ratio,lang) in skills" v-if="hovered">
+                    <span style="display: inline-block;"><span
+                        :style="'background-color: '+String((color_dict[lang]) ? color_dict[lang].color : 'red')+';color: '+String((color_dict[lang]) ? color_dict[lang].color : 'red')+';border-radius: 100px;'">・</span><span
+                        class="mr-2 ml-1">{{lang}}</span></span>
                 </template>
-            </div>
-            <template v-for="(ratio,lang) in skills" v-if="hovered">
-                <span style="display: inline-block;"><span :style="'background-color: '+String((color_dict[lang]) ? color_dict[lang].color : 'red')+';color: '+String((color_dict[lang]) ? color_dict[lang].color : 'red')+';border-radius: 100px;'">・</span><span class="mr-2 ml-1">{{lang}}</span></span>
-            </template>
-            <v-row>
-                <v-col cols="8">
-                    <v-icon>mdi-map-marker</v-icon>
-                    {{ raw_location }}
-                </v-col>
-                <v-col cols="4">
-                    <v-icon>mdi-star</v-icon>
-                    {{ stars }}
-                </v-col>
-                <v-col cols="8">
-                    <a :href="'https://github.com/'+user+'/'+repository" target="_blank" style="color: inherit; text-decoration: none;">
-                    <v-icon>mdi-github</v-icon>
-                    {{ repository }}</a>
-                </v-col>
-                <v-col cols="4">
-                    <v-icon>mdi-source-fork</v-icon>
-                    {{ forks }}
-                </v-col>
-            </v-row>
-        </v-card-text>
-    </v-card>
+                <v-row>
+                    <v-col cols="8">
+                        <v-icon>mdi-map-marker</v-icon>
+                        {{ raw_location }}
+                    </v-col>
+                    <v-col cols="4">
+                        <v-icon>mdi-star</v-icon>
+                        {{ stars }}
+                    </v-col>
+                    <v-col cols="8">
+                        <a :href="'https://github.com/'+user+'/'+repository" target="_blank"
+                           style="color: inherit; text-decoration: none;">
+                            <v-icon>mdi-github</v-icon>
+                            {{ repository }}</a>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-icon>mdi-source-fork</v-icon>
+                        {{ forks }}
+                    </v-col>
+                </v-row>
+            </v-card-text>
+        </v-card>
     </transition>
 </template>
 
@@ -74,7 +79,7 @@
             };
         },
         methods: {
-            clear(){
+            clear() {
                 this.url = "";
                 this.gif = "";
                 this.user = '';
@@ -84,9 +89,9 @@
                 this.forks = "";
                 this.skills = [];
             },
-            set(data){
-                this.gif="";
-                setTimeout(()=>{
+            set(data) {
+                this.gif = "";
+                setTimeout(() => {
                     this.url = data.url;
                     this.gif = data.gif;
                     this.user = data.user;
@@ -95,7 +100,7 @@
                     this.stars = data.stars;
                     this.forks = data.forks;
                     this.skills = JSON.parse(data.skills);
-                },300);
+                }, 300);
             },
         }
     };
